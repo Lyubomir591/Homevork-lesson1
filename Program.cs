4bee9e29f1c606ff21fe76a62b64b27a4f6c1053
Lyubomir <lyubomirbelov5178@gmail.com>
2022-09-16 23:17:57 +0300

    Код готов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число A:");
+int numberUserA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numberUserB = Convert.ToInt32(Console.ReadLine());
+int numbermax = 0;
+int numbermin = 0;
+if (numberUserA > numberUserB)
+{
+    numbermax = numberUserA;
+    numbermin = numberUserB;
+    Console.WriteLine($"Максимальное число: {numbermax}; Минимальное число: {numbermin}");
+
+}
+else
+{
+    numbermin = numberUserA;
+    numbermax = numberUserB;
+    Console.WriteLine($"Максимальное число: {numbermax}; Минимальное число: {numbermin}");
+};
