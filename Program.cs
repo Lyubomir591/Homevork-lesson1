2c4f60bdca5eae74a42564830d606fb7f9145dc2
Lyubomir591 <78299416+Lyubomir591@users.noreply.github.com>
2022-09-28 14:20:58 +0300

    Update Program.cs
    Дз обновлено до 3 го урока

--- Program.cs
+++ Program.cs
@@ -42,71 +42,113 @@
 // Homework Lessons2 
 
 // Задача 1 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-int numbers = Convert.ToInt32(Console.ReadLine());
-string number = numbers.ToString();
-Console.WriteLine(number[1]);
+// int numbers = Convert.ToInt32(Console.ReadLine());
+// string number = numbers.ToString();
+// Console.WriteLine(number[1]);
 
 
-// Задача 2
-int a = Convert.ToInt32(Console.ReadLine());
-int b = 10;
-if (a >= 100 && a <= 1000)
-{
-    a %= b;
-    Console.Write(a);
-}
-else if (a > 1000)
-{
-    string numb = a.ToString();
-    Console.WriteLine(numb[2]);
-}
-else
-{
-    Console.WriteLine("Нет  третьей цифры");
-}
-// Задача 3
+// // Задача 2
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = 10;
+// if (a >= 100 && a <= 1000)
+// {
+//     a %= b;
+//     Console.Write(a);
+// }
+// else if (a > 1000)
+// {
+//     string numb = a.ToString();
+//     Console.WriteLine(numb[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Нет  третьей цифры");
+// }
+// // Задача 3
 
-int[] arrDay = { 1, 2, 3, 4, 5, 6, 7 };
-Console.WriteLine("Введите цифру обозначающую день недели");
-int nuberDeyUser = Convert.ToInt32(Console.ReadLine());
-bool day = false;
-if (nuberDeyUser == 6 || nuberDeyUser == 7)
-{
-    day = true;
-}
+// int[] arrDay = { 1, 2, 3, 4, 5, 6, 7 };
+// Console.WriteLine("Введите цифру обозначающую день недели");
+// int nuberDeyUser = Convert.ToInt32(Console.ReadLine());
+// bool day = false;
+// if (nuberDeyUser == 6 || nuberDeyUser == 7)
+// {
+//     day = true;
+// }
 
-foreach (var item in arrDay)
+// foreach (var item in arrDay)
+// {
+//     if (item == nuberDeyUser)
+//     {
+//         if (day)
+//         {
+//             Console.WriteLine("Выходной день");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Рабочий день");
+//         }
+
+//     }
+// }
+// // Задача 4
+// int[] numbersArrUser = new int[8];
+
+// for (int counter = 0; counter < numbersArrUser.Length; counter++)
+// {
+//     Console.WriteLine($"Введите {counter + 1}-е число");
+//     numbersArrUser[counter] = Convert.ToInt32(Console.ReadLine());
+
+
+// }
+// int sum = 0;
+// foreach (var item in numbersArrUser)
+// {
+
+//     sum = item + sum;
+
+// }
+// Console.WriteLine($"Средняя {sum / numbersArrUser.Length}");
+
+
+//Урок 3 
+//Задание №1
+
+Console.WriteLine("Введите полиндром :");
+int numberPOlendrom = Convert.ToInt32(Console.ReadLine());
+void Revers(int number)
 {
-    if (item == nuberDeyUser)
+    int copyNumber = number;
+    int result = 0;
+    while (number != 0)
     {
-        if (day)
-        {
-            Console.WriteLine("Выходной день");
-        }
-        else
-        {
-            Console.WriteLine("Рабочий день");
-        }
-
+        int digit = number % 10;
+        result = result * 10 + digit;
+        number = number / 10;
+    }
+    if (result == copyNumber)
+    {
+        Console.WriteLine("Полиндром ");
+    }
+    else
+    {
+        Console.WriteLine("Не полиндром ");
     }
 }
-// Задача 4
-int[] numbersArrUser = new int[8];
+Revers(numberPOlendrom);
 
-for (int counter = 0; counter < numbersArrUser.Length; counter++)
+//  Задание № 3 
+Console.WriteLine("Введите число для подчета кубов ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+void Cub(int num)
 {
-    Console.WriteLine($"Введите {counter + 1}-е число");
-    numbersArrUser[counter] = Convert.ToInt32(Console.ReadLine());
-
-
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = i * i * i;
+        Console.Write($"{sum} ");
+    }
 }
-int sum = 0;
-foreach (var item in numbersArrUser)
-{
-
-    sum = item + sum;
-
-}
-Console.WriteLine($"Средняя {sum / numbersArrUser.Length}");
+Cub(min);
